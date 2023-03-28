5486a3f0f1bc6eec14523471487157d0b7d3bd74
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-03-28 18:11:36 +0900

    Zip() -> merging stream
    Zip을 통해 두 입력값이 모두 입력되어야 작동하도록 설계

---
+++ Assets/2.Scripts/Click2buttons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using TMPro;
+
+namespace UniRxPracitce
+{
+
+    public class Click2buttons : MonoBehaviour
+    {
+        [SerializeField] private Button btn1;
+        [SerializeField] private Button btn2;
+        [SerializeField] private Text txt;
+
+        void Start()
+        {
+            btn1.OnClickAsObservable().Subscribe(_ => { Debug.Log("Botton1 Clicked!"); });
+            btn2.OnClickAsObservable().Subscribe(_ => { Debug.Log("Botton2 Clicked!"); });
+
+            btn1.OnClickAsObservable().Zip(btn2.OnClickAsObservable(),(b1,b2) => "Clicked!").First().Repeat().SubscribeToText(txt, input => txt.text + input + "\n");
+        }
+
+    }
+}
