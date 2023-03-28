a10d694c0d2b1c3bd20e3184c7550bb402d15133
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-03-28 17:51:39 +0900

    Buffer, Skip -> tripleClick
    Buffer(3), Skip(2)을 통해 3회 입력 시 작동 구현

--- Assets/2.Scripts/ClickVisualize.cs
+++ Assets/2.Scripts/ClickVisualize.cs
@@ -14,7 +14,7 @@ namespace UniRxPracitce
         [SerializeField] private Button btn;    // [SerializeField]를 선언해두면 private변수를 inspecter창에서 접근 가능
         [SerializeField] private TextMeshProUGUI txt;   // TextMeshProUGUI를 써야 TextMeshPro관련 내용 조작 가능(text는 legacy가 되었으니 가능한 이걸 써보자)
 
-        [SerializeField] private int mode;  // 0 : change text if clicked, 1 : change text if clicked 3 times
+        [SerializeField] private int mode;  // 0 : change text if clicked, 1 : change text if clicked 3 times(use buffer), 2 : change text if clicked 3 times(use skip)
 
         void Start()
         {
@@ -30,16 +30,23 @@ namespace UniRxPracitce
                     txt.text = "Clicked";
                 });
             }
-            else if(mode == 1)
+            else if(mode == 1)          // Buffer(n) : 메세지를 n횟수만큼 모아서 송신
             {
                 btn.OnClickAsObservable().Buffer(3).Subscribe(input =>
                 {
                     txt.text = "Clicked";
                 });
             }
+            else if (mode == 2)         // Skip(n) : 메세지를 n횟수만큼 무시하고 이후 송신
+            {
+                btn.OnClickAsObservable().Skip(2).Subscribe(input =>
+                {
+                    txt.text = "Clicked";
+                });
+            }
 
             //btn.OnClickAsObservable().SubscribeToText(txt, input => "Clicked");       // unirx에는 이처럼 uGUI용 Observe와 Subscribe가 준비되어 있음
-                                                        //단, 현재 스크립트에서는 txt를 TextMeshProUGUI로 설정해두었으므로 사용불가, legacy의 text사용 시 가능
+            //단, 현재 스크립트에서는 txt를 TextMeshProUGUI로 설정해두었으므로 사용불가, legacy의 text사용 시 가능
         }
     }
 }
