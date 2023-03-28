70ba9f6d32720765cd590222d878f20e474a0ead
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-03-28 17:45:21 +0900

    buffer() -> triple Click
    buffer를 이용해 입력 횟수를 판별

--- Assets/2.Scripts/ClickVisualize.cs
+++ Assets/2.Scripts/ClickVisualize.cs
@@ -5,6 +5,7 @@ using UniRx;
 using UnityEngine.UI;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using TMPro;
+using System;
 
 namespace UniRxPracitce
 {
@@ -12,13 +13,30 @@ namespace UniRxPracitce
     {
         [SerializeField] private Button btn;    // [SerializeField]를 선언해두면 private변수를 inspecter창에서 접근 가능
         [SerializeField] private TextMeshProUGUI txt;   // TextMeshProUGUI를 써야 TextMeshPro관련 내용 조작 가능(text는 legacy가 되었으니 가능한 이걸 써보자)
-        
+
+        [SerializeField] private int mode;  // 0 : change text if clicked, 1 : change text if clicked 3 times
+
         void Start()
         {
-            btn.onClick.AsObservable().Subscribe(input =>
+            btn.OnClickAsObservable().Subscribe(input =>
             {
-                txt.text = "Clicked";
+                Debug.Log("Clicked!");
             });
+
+            if (mode == 0)
+            {
+                btn.onClick.AsObservable().Subscribe(input =>
+                {
+                    txt.text = "Clicked";
+                });
+            }
+            else if(mode == 1)
+            {
+                btn.OnClickAsObservable().Buffer(3).Subscribe(input =>
+                {
+                    txt.text = "Clicked";
+                });
+            }
 
             //btn.OnClickAsObservable().SubscribeToText(txt, input => "Clicked");       // unirx에는 이처럼 uGUI용 Observe와 Subscribe가 준비되어 있음
                                                         //단, 현재 스크립트에서는 txt를 TextMeshProUGUI로 설정해두었으므로 사용불가, legacy의 text사용 시 가능
