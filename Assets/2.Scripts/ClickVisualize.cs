d8deaa30161d2e0cdf08b2a69f398f37abe60fa1
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-03-28 17:05:50 +0900

    Update ClickVisualize.cs

--- Assets/2.Scripts/ClickVisualize.cs
+++ Assets/2.Scripts/ClickVisualize.cs
@@ -11,7 +11,7 @@ namespace UniRxPracitce
     public class ClickVisualize : MonoBehaviour
     {
         [SerializeField] private Button btn;    // [SerializeField]를 선언해두면 private변수를 inspecter창에서 접근 가능
-        [SerializeField] private TextMeshProUGUI txt;
+        [SerializeField] private TextMeshProUGUI txt;   // TextMeshProUGUI를 써야 TextMeshPro관련 내용 조작 가능(text는 legacy가 되었으니 가능한 이걸 써보자)
         
         void Start()
         {
