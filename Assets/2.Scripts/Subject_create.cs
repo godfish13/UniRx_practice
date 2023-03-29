251ee9090c876001fbc05399ca37339513c9a4e7
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-03-30 05:24:58 +0900

    Creating Subject practice
    Subject 타입 객체를 만들고 해당 Subject의 스트림을 만든 후 이벤트를 전달해보기 연습

---
+++ Assets/2.Scripts/Subject_create.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class Subject_create : MonoBehaviour
+{
+    Subject<string> sub = new Subject<string>();
+
+    WaitForSeconds ws = new WaitForSeconds(3f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sub.Subscribe(_ => Debug.Log("sub1 : " + _));
+        sub.Subscribe(_ => Debug.Log("sub2 : " + _));
+        StartCoroutine(Praticer());
+    }
+
+    IEnumerator Praticer()
+    {
+        yield return ws;
+        sub.OnNext("Hello, World!");
+        yield return ws;
+        sub.OnNext("Bye, World!");
+    }
+}
