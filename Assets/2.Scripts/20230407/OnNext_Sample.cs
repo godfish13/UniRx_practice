f832ba0379187fd121aca61ac2e87f134a937de3
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-04-09 00:02:50 +0900

    20230409 AddTo practive added

--- Assets/2.Scripts/20230407/OnNext_Sample.cs
+++ Assets/2.Scripts/20230407/OnNext_Sample.cs
@@ -7,7 +7,7 @@ public class OnNext_Sample : MonoBehaviour
     Subject<object> s = new Subject<object>();
     void Start()
     {
-        s.Subscribe(_ => { Debug.Log("message : " + _); });
+        s.Subscribe(_ => { Debug.Log("message : " + _); }).AddTo(this); //지정한 GameObject(this)가 파기되면 이 스트림을 Dispose 한다.
         StartCoroutine(LetsGo());
         s.OnNext("Im in Start bebe!");
         s.OnNext("Second Start bebe!");
